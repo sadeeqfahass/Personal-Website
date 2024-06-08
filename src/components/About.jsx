9fea0bc4578fee50591efd5720e6bd2c5385357f
sadeeq fahass <sadeeqfhassan@gmail.com>
2024-06-08 15:39:37 +0100

    #

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ function About() {
       </div>
       <div className="about-content">
         <p>
-          I am Abubakar (Sadeeq) Farouq, an enthusiastic Frontend developer from in Kano state, Nigeria. I find joy in writing code and in the process of building beautiful websites and web applications that solve real-world problems and provide exceptional user experiences. <br /> I hold a BSc in Computer Systems and Information Technology,  and love to share my knowledge with others. Aside being a Frontend developer, I derive great satisfaction from helping people learn to code and empowering them to create amazing digital solutions.
+          I am Abubakar (Sadeeq) Farouq, an enthusiastic Frontend developer from Kano state, Nigeria. I find joy in writing code and in the process of building beautiful websites and web applications that solve real-world problems and provide exceptional user experiences. <br /> I hold a BSc in Computer Systems and Information Technology,  and love to share my knowledge with others. Aside being a Frontend developer, I derive great satisfaction from helping people learn to code and empowering them to create amazing digital solutions.
         </p>
       </div>
       <div className="btn">
